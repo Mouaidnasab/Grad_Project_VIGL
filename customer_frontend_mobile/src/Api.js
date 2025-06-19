d3fe534b5e0a5b2211d1149c613a7247b5a067e8
Mouaidnasab <65204173+Mouaidnasab@users.noreply.github.com>
2025-06-19 03:11:19 +0300

    minor fix

--- customer_frontend_mobile/src/Api.js
+++ customer_frontend_mobile/src/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const GOVERNMENT_IP =
   process.env.REACT_APP_GOV_BACKEND_URL || "http://192.168.0.102:8001";
 
-const api = axios.create({
-  baseURL: GOVERNMENT_IP.replace(/\/+$/, "/"),
+export default axios.create({
+  baseURL: GOVERNMENT_IP,
   headers: { "Content-Type": "application/json" },
 });
